520d3540c76033757f541f961b1e376bfeb31323
snack251543 <130037024+snack251543@users.noreply.github.com>
2023-04-06 14:54:28 +0700

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt 
+import plotly.express as px
+import plotly.graph_objects as graph_objects
+st.header("ii")
